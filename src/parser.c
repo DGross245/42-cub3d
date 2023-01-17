91b69281629828b816a741c9cb3108a9054caeb1
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-01-17 07:09:31 +0100

    commit

--- src/parser.c
+++ src/parser.c
@@ -3,20 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:45:40 by dgross            #+#    #+#             */
-/*   Updated: 2023/01/16 16:01:19 by dgross           ###   ########.fr       */
+/*   Updated: 2023/01/17 07:09:21 by dna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+#include <fcntl.h> // open
+
 int	parser(int argc, char *argv)
 {
+	int	fd;
+
 	if (argc < 2)
 	{
 		print_error("Map in missing ❗");
 		return (ERROR);
 	}
+	if (argc > 2)
+	{
+		print_error("too many arguments❗");
+		return (ERROR);
+	}
+	fd = open(argv[1]);
 }
