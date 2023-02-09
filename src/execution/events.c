2a57ac52d8f0ea4de69aafa82ade5a1c5ab8bb05
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-02-09 18:23:40 +0100

    added sprinting

--- src/execution/events.c
+++ src/execution/events.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 09:47:35 by dna               #+#    #+#             */
-/*   Updated: 2023/02/08 12:14:24 by dna              ###   ########.fr       */
+/*   Updated: 2023/02/09 18:14:10 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,44 @@
 #include "cub3d.h"
 #include "macros.h"
 
+static void	go(t_cub3d *cube)
+{
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_W)
+		&& mlx_is_key_down(cube->mlx, MLX_KEY_LEFT_SHIFT))
+		cube->mov_speed = 0.15;
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_W))
+		go_up(&cube->player, cube);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_S))
+		go_down(&cube->player, cube);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_A))
+		go_left(&cube->player, cube);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_D))
+		go_right(&cube->player, cube);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_LEFT))
+		turn_left(&cube->player);
+	else if (mlx_is_key_down(cube->mlx, MLX_KEY_RIGHT))
+		turn_right(&cube->player);
+}
+
+static	void	duck(t_cub3d *cube)
+{
+	cube->player.plane_x = 0.5;
+}
+
 void	events(void *param)
 {
 	t_cub3d	*cube;
 
 	cube = (t_cub3d *)param;
+	cube->mov_speed = 0.08;
 	mlx_set_window_limit(cube->mlx, WIDTH, HEIGHT, WIDTH, HEIGHT);
 	if (mlx_is_key_down(cube->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(cube->mlx);
-	if ((mlx_is_key_down(cube->mlx, MLX_KEY_W)
-			|| mlx_is_key_down(cube->mlx, MLX_KEY_UP)))
-		go_up(&cube->player, cube);
-	if ((mlx_is_key_down(cube->mlx, MLX_KEY_S)
-			|| mlx_is_key_down(cube->mlx, MLX_KEY_DOWN)))
-		go_down(&cube->player, cube);
-	if (mlx_is_key_down(cube->mlx, MLX_KEY_A)
-		|| mlx_is_key_down(cube->mlx, MLX_KEY_LEFT))
-		go_left(&cube->player, cube);
-	if (mlx_is_key_down(cube->mlx, MLX_KEY_D)
-		|| mlx_is_key_down(cube->mlx, MLX_KEY_RIGHT))
-		go_right(&cube->player, cube);
-	else if (mlx_is_key_down(cube->mlx, MLX_KEY_E))
+	go(cube);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_E))
 		door(cube, &cube->player);
+	if (mlx_is_key_down(cube->mlx, MLX_KEY_LEFT_CONTROL))
+		duck(cube);
 	mouse_position(cube, &cube->player);
 	redraw(cube);
 }
--- src/execution/open.c
+++ src/execution/open.c
@@ -6,22 +6,12 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 10:13:26 by dna               #+#    #+#             */
-/*   Updated: 2023/02/09 15:00:13 by dgross           ###   ########.fr       */
+/*   Updated: 2023/02/09 15:50:52 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42.h"
 #include "execution.h"
-
-static void	open_door(t_cub3d *cube, double tmp_x, double tmp_y)
-{
-	cube->data.map[(int)tmp_x][(int)tmp_y] = '3';
-}
-
-static void	close_door(t_cub3d *cube, double tmp_x, double tmp_y)
-{
-	cube->data.map[(int)tmp_x][(int)tmp_y] = '2';
-}
 
 void	door(t_cub3d *cube, t_cords *player)
 {
@@ -34,13 +24,13 @@ void	door(t_cub3d *cube, t_cords *player)
 		tmp_x = ((player->xppos) + player->xpdir * 0.08);
 		tmp_y = ((player->yppos) + player->ypdir * 0.08);
 		if (cube->data.map[(int)tmp_x - 1][(int)tmp_y] == '2')
-			open_door(cube, tmp_x - 1, tmp_y);
+			cube->data.map[(int)tmp_x - 1][(int)tmp_y] = '3';
 		else if (cube->data.map[(int)tmp_x + 1][(int)tmp_y] == '2')
-			open_door(cube, tmp_x + 1, tmp_y);
+			cube->data.map[(int)tmp_x + 1][(int)tmp_y] = '3';
 		else if (cube->data.map[(int)tmp_x - 1][(int)tmp_y] == '3')
-			close_door(cube, tmp_x - 1, tmp_y);
+			cube->data.map[(int)tmp_x - 1][(int)tmp_y] = '2';
 		else if (cube->data.map[(int)tmp_x + 1][(int)tmp_y] == '3')
-			close_door(cube, tmp_x + 1, tmp_y);
+			cube->data.map[(int)tmp_x + 1][(int)tmp_y] = '2';
 	}
 	else
 		cube->door++;
