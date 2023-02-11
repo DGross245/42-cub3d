c530a5a82e47b9307f63bb16d22a98575a671677
Daniel Gross <dgross@2-H-7.42heilbronn.de>
2023-02-11 12:13:02 +0100

    fixed wall problem, but doors doesnt work anymore

--- src/execution/go.c
+++ src/execution/go.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   go.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 09:44:34 by dna               #+#    #+#             */
-/*   Updated: 2023/02/11 08:45:03 by dna              ###   ########.fr       */
+/*   Updated: 2023/02/11 09:53:36 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,28 @@
 #include "cub3d.h"
 #include "libft.h"
 
-#include <math.h>
-#include <stdio.h>
+static char	check_wall_hit(t_cub3d *cube, double tmp_x, double tmp_y)
+{
+	if (cube->data.map[(int)(tmp_x + 0.2)][(int)(tmp_y + 0.2)] == '0' &&
+		cube->data.map[(int)(tmp_x - 0.2)][(int)(tmp_y - 0.2)] == '0' &&
+		cube->data.map[(int)(tmp_x - 0.2)][(int)(tmp_y + 0.2)] == '0' &&
+		cube->data.map[(int)(tmp_x + 0.2)][(int)(tmp_y - 0.2)] == '0')
+		return ('0');
+	else
+		return ('1');
+}
 
 void	go_up(t_cords *player, t_cub3d *cube)
 {
 	double	tmp_x;
 	double	tmp_y;
-	int		x;
-	int		y;
 
 	tmp_x = player->xppos + cube->mov_speed * player->xpdir;
 	tmp_y = player->yppos + cube->mov_speed * player->ypdir;
-	x = round(tmp_x);
-	y = round(tmp_y);
-	if (x >= 0 && x < (int)ft_strlen(cube->data.map[x]) && y >= 0 && y < cube->data.height)
+	if (check_wall_hit(cube, tmp_x, tmp_y) == '0')
 	{
-		if (cube->data.map[x][y] == '0')
-		{
-			player->xppos = tmp_x;
-			player->yppos = tmp_y;
-		}	
+		player->xppos = tmp_x;
+		player->yppos = tmp_y;
 	}
 }
 
@@ -43,9 +44,9 @@ void	go_down(t_cords *player, t_cub3d *cube)
 	double	tmp_x;
 	double	tmp_y;
 
-	tmp_x = player->xppos - player->xpdir * cube->mov_speed;
-	tmp_y = player->yppos - player->ypdir * cube->mov_speed;
-	if (cube->data.map[(int)tmp_x][(int)tmp_y] == '0')
+	tmp_x = player->xppos - cube->mov_speed * player->xpdir;
+	tmp_y = player->yppos - cube->mov_speed * player->ypdir;
+	if (check_wall_hit(cube, tmp_x, tmp_y) == '0')
 	{
 		player->xppos = tmp_x;
 		player->yppos = tmp_y;
@@ -57,9 +58,9 @@ void	go_left(t_cords *player, t_cub3d *cube)
 	double	tmp_x;
 	double	tmp_y;
 
-	tmp_x = player->xppos - player->ypdir * cube->mov_speed;
-	tmp_y = player->yppos + player->xpdir * cube->mov_speed;
-	if (cube->data.map[(int)tmp_x][(int)tmp_y] == '0')
+	tmp_x = player->xppos - cube->mov_speed * player->ypdir;
+	tmp_y = player->yppos + cube->mov_speed * player->xpdir;
+	if (check_wall_hit(cube, tmp_x, tmp_y) == '0')
 	{
 		player->xppos = tmp_x;
 		player->yppos = tmp_y;
@@ -71,11 +72,12 @@ void	go_right(t_cords *player, t_cub3d *cube)
 	double	tmp_x;
 	double	tmp_y;
 
-	tmp_x = player->xppos + player->ypdir * cube->mov_speed;
-	tmp_y = player->yppos - player->xpdir * cube->mov_speed;
-	if (cube->data.map[(int)tmp_x][(int)tmp_y] == '0')
+	tmp_x = player->xppos + cube->mov_speed * player->ypdir;
+	tmp_y = player->yppos - cube->mov_speed * player->xpdir;
+	if (check_wall_hit(cube, tmp_x, tmp_y) == '0')
 	{
 		player->xppos = tmp_x;
 		player->yppos = tmp_y;
 	}
 }
+
