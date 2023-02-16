b7a31d6e8ab7c6465c150c9b77bec3bdf17f23d8
Daniel Gross <dgross@3-B-11.42heilbronn.de>
2023-02-16 14:11:29 +0100

    update

--- src_bonus/execution_bonus/go_bonus.c
+++ src_bonus/execution_bonus/go_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:28:36 by dgross            #+#    #+#             */
-/*   Updated: 2023/02/11 12:11:54 by dgross           ###   ########.fr       */
+/*   Updated: 2023/02/14 09:01:51 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ char	check_wall_hit(t_cub3d *cube, double tmp_x, double tmp_y)
 		cube->data.map[(int)(tmp_x - 0.2)][(int)(tmp_y + 0.2)] == '0' &&
 		cube->data.map[(int)(tmp_x + 0.2)][(int)(tmp_y - 0.2)] == '0')
 		return ('0');
-	else if (cube->data.map[(int)(tmp_x)][(int)(tmp_y)] == '3' ||
-		cube->data.map[(int)(tmp_x)][(int)(tmp_y)] == '0')
+	else if (cube->data.map[(int)(tmp_x)][(int)(tmp_y)] == '3')
 		return ('3');
 	else if (cube->data.map[(int)(tmp_x + 0.2)][(int)(tmp_y + 0.2)] == '2' &&
 		cube->data.map[(int)(tmp_x - 0.2)][(int)(tmp_y - 0.2)] == '2' &&
